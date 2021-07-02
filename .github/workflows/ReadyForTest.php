791f4f439b706eabe99a9ce74690bf998c15f30a
Artem Alexeev <Alexeev2013@yandex.ru>
2021-07-02 11:02:53 +0300

    [ALDEV-1] - updated work 69

--- .github/workflows/ReadyForTest.php
+++ .github/workflows/ReadyForTest.php
@@ -9,17 +9,17 @@ jobs:
     steps:
       - name: Echo github
         #if: github.event.review.state == 'approved' && github.event.pull_request.mergeable == 'true'
-        run: echo "state - ${{ toJson(github) }} "
+        run: echo "state - ${{ toJson(github) }} " # no mergable properties
 
       - name: Echo event
         #if: github.event.review.state == 'approved' && github.event.pull_request.mergeable == 'true'
         run: echo "state - ${{ github.event.review.state }} --- branch - ${{ toJson(github.head_ref) }}"
 
-      - name: Echo pull_request
+      - name: Echo pull_request # no mergeble
         run: echo "${{ toJson(github.event.pull_request) }} "
 
-      - name: Echo mergeable
+      - name: Echo mergeable # all empty
         run: echo "${{ github.event.pull_request.mergeable }} --- ${{ github.event.mergeable }} ---  ${{ github.mergeable }}"
 
-      - name: Branch
-        run: echo " --- branch - ${{ toJson(github.head_ref) }}"
+      - name: Branch head
+        run: echo " --- branch - ${{ toJson(github.event.pull_request.head.ref) }}" # empty
--- .github/workflows/test_pull.php
+++
@@ -1,9 +0,0 @@
-name: Test status
-on:
-  pull_request:
-jobs:
-  test-status-job:
-    runs-on: ubuntu-latest
-    steps:
-      - name: debug github object On pull
-        run: echo "${{ toJson(github) }}"
